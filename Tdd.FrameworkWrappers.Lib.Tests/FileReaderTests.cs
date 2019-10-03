95114cb3092b3dac5ce19e7a08fea11dad49c0f4
Edward Mays <azure-tdl@copperstarsystems.com>
2019-10-03 11:01:39 -0500

    Initial, difficult-to-test implementation

---
+++ Tdd.FrameworkWrappers.Lib.Tests/FileReaderTests.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace Tdd.FrameworkWrappers.Lib.Tests
+{
+    [TestFixture]
+    public class FileReaderTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            systemUnderTest = new FileReader();
+        }
+
+        private FileReader systemUnderTest;
+
+        private void CleanupTestFile(string filePath)
+        {
+            File.Delete(filePath);
+        }
+
+        private void CreateTestFile(string filePath, string fileContent)
+        {
+            var directory = Path.GetDirectoryName(filePath);
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+
+            File.WriteAllText(filePath, fileContent);
+        }
+
+        [Test]
+        public void ReadText_Always_PerformsExpectedWork()
+        {
+            // This test can't actually test anything because the SystemUnderTest uses the static File
+            // class - in other words, we can't mock File to verify that ReadAllText is called.
+        }
+
+        [Test]
+        public void ReadText_WhenFileExists_ReturnsFileContents()
+        {
+            const string filePath = @"c:\temp\test.txt";
+            const string fileContent = "The quick brown fox jumps over the lazy dog.";
+            CreateTestFile(filePath, fileContent);
+            var result = systemUnderTest.ReadText(filePath);
+            Assert.That(result, Is.EqualTo(fileContent));
+            CleanupTestFile(filePath);
+        }
+    }
+}
---
+++ Tdd.FrameworkWrappers.Lib/FileReader.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Net;
+
+namespace Tdd.FrameworkWrappers.Lib
+{
+    public class FileReader
+    {
+        public string ReadText(string filePath)
+        {
+            return File.ReadAllText(filePath);
+        }
+    }
+}
